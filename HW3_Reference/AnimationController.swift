e4e24a0750cca4dee0ad389e0213c13e0119ae70
Emil Shpeklord <emil42661@gmail.com>
2024-11-04 13:42:46 +0300

    preparing

--- HW3_Reference/AnimationController.swift
+++ HW3_Reference/AnimationController.swift
@@ -8,24 +8,4 @@ import UIKit
 import Lottie
 
 class AnimationController: UIViewController {
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        playAnimation()
-    }
-    func playAnimation() {
-        guard let nameAnimation = animationTitle else { return }
-        guard let speedAnimation = animationSpeed else { return }
-        guard let colorAnimation = animationColor else { return }
-        let animationView = LottieAnimationView(name: nameAnimation)
-        animationView.frame = self.view.bounds
-        animationView.backgroundColor = colorAnimation
-        view.addSubview(animationView)
-        animationView.animationSpeed = speedAnimation
-        animationView.play {_ in
-            self.navigationController?.popViewController(animated: false)
-        }
-    }
-
 }
